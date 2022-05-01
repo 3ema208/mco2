8257e321698f014cd6da617ed6bd668f9779ba85
nedobytko_r <nedobytko.r@gmail.com>
2022-05-01 12:10:53 +0300

    add sender to telegram

--- src/main.rs
+++ src/main.rs
@@ -1,83 +1,42 @@
-use serialport::{Error, ErrorKind, SerialPort};
-use std::io::ErrorKind as ioErrKind;
-use std::thread::sleep;
 use std::time::Duration;
+use std::env;
+use teloxide::{
+    prelude::*,
+    types::{ChatId},
+};
 
-const START_COMMAND: u8 = 0x11;
-const START_RESPONSE: u8 = 0x16;
+mod sensor;
+use sensor::SensorCo2;
+use tokio::time::sleep;
 
- 
-#[derive(Clone)]
-enum Command {
-    ReadMeasure = 0x01,
-    Calibration = 0x03,
-    AbcParameterCheck = 0x0F,
-    AbcParameterSet = 0x10,
-}
-
-struct SensorCo2 {
-    serial: Box<dyn SerialPort>,
-    buff: Vec<u8>,
-}
-
-fn calculate_crc(data: &[u8]) -> u8 {
-    let sum = data.iter().sum::<u8>();
-    255u8 - sum + 1
-}
-
-fn check_crc(data: &[u8], crc: &u8) -> bool {
-    calculate_crc(data) == *crc
-}
-
-impl SensorCo2 {
-    fn new(port: String) -> Result<Self, Error> {
-        let port_builder = serialport::new(port, 9_600).timeout(Duration::from_secs(5));
-        let port = port_builder.open()?;
-        Ok(Self {
-            serial: port,
-            buff: vec![0; 8],
-        })
-    }
-
-    fn pack_command(data: Vec<u8>) -> Vec<u8> {
-        let mut r = vec![];
-        r.push(START_COMMAND);
-        r.push(data.len() as u8);
-        r.extend(data.iter());
-        r.push(calculate_crc(&r[..]));
-        r
-    }
-
-    fn read_response(&mut self) -> Result<&[u8], Error> {
-        let size = self.serial.read(&mut self.buff)?;
-        if size == 0 {
-            return Err(Error::new(ErrorKind::Io(ioErrKind::Other), "Can't read"));
+#[tokio::main]
+async fn main() {
+    let chat_id: i64 = env::var("CHAT_ID").unwrap().parse::<i64>().unwrap();
+    let uart_port = env::var("UART_PORT").unwrap();
+    let bot_token = env::var("BOT_TOKEN").unwrap();
+    
+    
+    pretty_env_logger::init();
+    let mut device = SensorCo2::new(uart_port).unwrap();
+    log::info!("Starting throw dice bot...");
+    let bot = Bot::new(bot_token).auto_send();
+        
+    let chat = ChatId(chat_id);
+    let last_level = 0;
+    loop {
+        match device.get_co2_value() {
+            Ok(e) => {
+                if e - last_level > 200 {
+                    let message = format!("Co2 level {}", e);
+                    bot.send_message(chat, message).await.unwrap();
+                }
+            }
+            Err(e) => {
+                let message = format!("Err measure co2, {}", e);
+                bot.send_message(chat, message).await.unwrap();
+            }
         }
-        let pack = &self.buff[..size - 1];
-        let crc = self.buff.last().unwrap();
-        if check_crc(pack, crc) {
-            let params = pack[1] as usize;
-            Ok(&self.buff[3..3 + params - 1])
-        } else {
-            Err(Error::new(ErrorKind::Io(ioErrKind::Other), "Wrong crc"))
-        }
-    }
-
-    fn get_co2_value(&mut self) -> Result<i32, Error> {
-        let command = SensorCo2::pack_command(vec![Command::ReadMeasure as u8]);
-        self.serial.write_all(&command)?;
-        let pack = self.read_response()?;
-        let df1 = pack[0] as i32;
-        let df2 = pack[1] as i32;
-        Ok(df1 * 256 + df2)
+        sleep(Duration::from_secs(120)).await;
     }
 }
-
-fn main() {
-    let mut p = SensorCo2::new(String::from("/dev/tty.usbserial-14330")).unwrap();
-    for _ in 0..10 {
-        let co2_lvl = p.get_co2_value().unwrap();
-        println!("Level co2: {:?}", co2_lvl);
-        sleep(Duration::from_secs(120))
-    }
-}
+// })
