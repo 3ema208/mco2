6567e8a233924a24e3a2daebc52f8fdd1d046d3c
Nedobytko <nedobytko.r@ajax.systems>
2022-12-27 17:28:32 +0200

    get avaliable uart port

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,65 @@
-use std::env;
+use std::{env, fmt::Display};
+use std::error::Error;
 use std::time::Duration;
 use teloxide::{prelude::*, types::ChatId};
+use serialport::{available_ports, SerialPortType};
+#[allow(dead_code)]
 
 mod sensor;
 use sensor::SensorCo2;
 use tokio::time::sleep;
 
-const LIMIT: i32 = 100;
+const LIMIT: i32 = 200;
+const UART_LOOKING: u16 = 6790;
+
+
+#[derive(Debug, Clone)]
+struct NotFoundPortName;
+
+impl Error for NotFoundPortName {}
+
+
+impl Display for NotFoundPortName {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Port is not found")
+    }
+}
+
+
+fn get_uart_port_name() -> Result<String, Box<dyn Error>> {
+    let ports = available_ports()?;
+    for port in ports.into_iter() {
+        match port.port_type {
+            SerialPortType::UsbPort(port_info) => {
+                if port_info.vid == UART_LOOKING {
+                    return Ok(port.port_name)
+                }
+            },
+            _ => {},
+        }
+    }
+    Err(Box::new(NotFoundPortName))
+}
+
 
 #[tokio::main]
 async fn main() {
-    let chat_id: i64 = env::var("CHAT_ID").unwrap().parse::<i64>().unwrap();
-    let uart_port = env::var("UART_PORT").unwrap();
-    let bot_token = env::var("BOT_TOKEN").unwrap();
+    let chat_id: i64 = match env::var("CHAT_ID") {
+        Ok(c) => c.parse::<i64>().unwrap(),
+        Err(_) => 292317891,
+    };
 
-    let mut device = SensorCo2::new(uart_port).unwrap();
+    let uart_port: String = get_uart_port_name().unwrap();
+
+    let bot_token = match env::var("BOT_TOKEN"){
+        Ok(v) => v,
+        Err(_) => "5379849529:AAF72rf4rl0cCv4nzK-jNXT575niY_RXBDY".to_string(),
+    };
+
+    let mut device = match SensorCo2::new(uart_port) {
+        Ok(device) => device,
+        Err(err) => panic!("{}", err)
+    };
 
     let bot = Bot::new(bot_token).auto_send();
     let chat = ChatId(chat_id);
@@ -24,15 +69,23 @@ async fn main() {
             Ok(e) => {
                 if (e - last_level).abs() > LIMIT {
                     let message = format!("Co2 level {}", e);
-                    bot.send_message(chat, message).await.unwrap();
+                    let rs = bot.send_message(chat, message).await;
+                    match rs {
+                        Ok(_) => {},
+                        Err(e) => { println!("{}", e)}
+                    }
                     last_level = e;
                 }
             }
             Err(e) => {
                 let message = format!("Err measure co2, {}", e);
-                bot.send_message(chat, message).await.unwrap();
+                let rs = bot.send_message(chat, message).await;
+                match rs {
+                    Ok(_) => {},
+                    Err(e) => { println!("{}", e)}
+                }
             }
         }
-        sleep(Duration::from_secs(120)).await;
+        sleep(Duration::from_secs(5 * 60)).await;
     }
 }
